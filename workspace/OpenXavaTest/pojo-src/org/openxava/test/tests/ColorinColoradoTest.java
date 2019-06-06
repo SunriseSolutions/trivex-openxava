b6a4ccc0b2f2cfb14717ac5bfd17d9e7ad4f3a0b
binhle410 <binh@sunrise.vn>
2019-06-06 17:41:14 +0700

    first commit

---
+++ workspace/OpenXavaTest/pojo-src/org/openxava/test/tests/ColorinColoradoTest.java
@@ -0,0 +1,23 @@
+package org.openxava.test.tests;
+
+import org.openxava.tests.*;
+
+/**
+ * @author Javier Paniza
+ */
+
+public class ColorinColoradoTest extends ModuleTestBase {
+	
+	public ColorinColoradoTest(String testName) {
+		super(testName, "ColorinColorado");		
+	}
+	
+	public void testAfterEachRequestAction_permalinkNotForRegularActions() throws Exception {  
+		assertValue("name", "NULLCOLORADO"); 
+		setValue("name", "");
+		execute("ColorinColorado.fillName");		
+		assertValue("name", "COLORIN COLORADO");
+		HtmlUnitUtils.assertPageURI(getHtmlPage(), "/ColorinColorado"); 
+	}
+	
+}
